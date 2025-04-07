2968a9085bef27efef1a322f6cc229305d060317
Codigo_Rosa <marianacova33@gmail.com>
2025-04-07 10:54:39 -0400

    allowed host

--- blogevacovarte/settings.py
+++ blogevacovarte/settings.py
@@ -128,6 +128,8 @@ import os
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')   #ruta a la raiz del proyecto y se une con el directorio media.RUTA
 
+
+
 #subida de mi repositori railway
 ALLOWED_HOSTS=['localhost','web-production-7402.up.railway.app']#localhost para que se pueda ver en desarrollo
 #ALLOWED_HOSTS=['*'] cualquiera podria ser el dominio de esta app

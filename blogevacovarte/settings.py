b4e75ef750668d400d2b9df8fb76e2686f32e2db
Codigo_Rosa <marianacova33@gmail.com>
2025-04-08 13:25:15 -0400

    primer commit

--- blogevacovarte/settings.py
+++ blogevacovarte/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import blogevacovarte
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'articulos',
     'tinymce',
+    blogevacovarte,
 ]
 
 MIDDLEWARE = [
